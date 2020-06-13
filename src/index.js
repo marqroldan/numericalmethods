f2afcb04fe990fd5d84b1701d7e1b275fa84d1b3
Marq Roldan <hcmlroldan@gmail.com>
2020-06-14 01:45:34 +0800

    Fixed bad style import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@Styles/global';
+import '@Styles/global.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
