03e77f9ff53253ee7bdc1421ab4ad92329b7d129
Marq Roldan <hcmlroldan@gmail.com>
2020-06-14 01:16:52 +0800

    Fixed bad test for sass files

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
 
       // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
       {
-        test: /\.(s)css$/,
+        test: /\.(sa|sc|c)ss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
       { test: /\.(png|jpg|jpeg|gif)$/, loader: 'url-loader?limit=100000' },
