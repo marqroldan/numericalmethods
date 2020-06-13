3f8c5751d6f5e47b566549e6d8e912bf9052feb2
Marq Roldan <hcmlroldan@gmail.com>
2020-06-14 01:44:27 +0800

    Updated index files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
+import '@Styles/main.scss';
+
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import RegulaFalsi from '@Methods/regulafalsi';
 import Secant from '@Methods/secant';
 import Bisection from '@Methods/bisection';
 
+import Header from '@Components/Header';
+import Main from '@Components/Main';
+
 function App() {
 
-  const method = new Secant();
+  const METHOD = Bisection;
+  const method = new METHOD();
   method.coefficients = [
     -10,1,-4,1
   ];
-  
-  method.process(2, 3);
+
+  method.roundingRules = 4;
+
+
+  method.process(5, 6);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="_container">
+      <Header>
+        Inserted text
+      </Header>
+      <Main>
+
+      </Main>
     </div>
   );
 }
