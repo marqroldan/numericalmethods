7c715dc3347e6b469e22851adabb4a7faca49557
Marq Roldan <hcmlroldan@gmail.com>
2020-06-14 01:46:26 +0800

    Added dummy styling and render for Main body

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+import './styles.scss';
+import React from 'react';
+
+
+export default class Main extends React.PureComponent {
+    render() {
+        return (<main className={'mainPrimary'}>hey</main>)
+    }
+}
