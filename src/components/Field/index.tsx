108f46b46e5b018b1ca2d5bea7ceec50c539c5a3
Marq Roldan <hcmlroldan@gmail.com>
2020-06-14 14:21:22 +0800

    Updated field component to allow labels for element and functions

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -3,19 +3,22 @@ import React from 'react';
 import Text from '@Components/Text';
 
 interface Props {
-  label: string;
+  label: string | Function | JSX.Element;
 }
 
 export default class Field extends React.PureComponent<
   Props & React.HTMLAttributes<HTMLDivElement>
 > {
   render() {
+    const { label, children } = this.props;
     return (
       <div className={'field'}>
         <div className={'field__labelHolder'}>
-          <Text className={'label'}>{this.props.label}</Text>
+          <Text className={'label'}>
+            {typeof label === 'function' ? label() : label}
+          </Text>
         </div>
-        <div className={'field__childrenHolder'}>{this.props.children}</div>
+        <div className={'field__childrenHolder'}>{children}</div>
       </div>
     );
   }
