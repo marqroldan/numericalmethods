f6126ff2057a447d09e263952f9bda520dea72a5
Marq Roldan <hcmlroldan@gmail.com>
2020-06-14 14:25:29 +0800

    Removed extra text and adjusted stylling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,7 @@ import './styles.scss';
 import React from 'react';
 
 export default class Header extends React.PureComponent {
-    render() {
-        return (
-            <header className={'headerMain'} >
-                hi!
-                {this.props.children}
-            </header>
-        )
-    }
+  render() {
+    return <header className={'headerMain'}>{this.props.children}</header>;
+  }
 }
-
