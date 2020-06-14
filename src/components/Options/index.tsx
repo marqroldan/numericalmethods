63331fa990c3197adeaf1b0b71e73b0559a003be
Marq Roldan <hcmlroldan@gmail.com>
2020-06-14 19:20:11 +0800

    Added styling and showing of option items

--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,14 +1,37 @@
 import './styles.scss';
 import React from 'react';
 
-interface State {
-  expanded: boolean;
+interface Props {
+  options: {
+    [key: string]: any;
+  }[];
+  selectedValue: string | number;
+  onChange: Function;
 }
 
-export default class Options extends React.PureComponent<{}, State> {
+interface State {
+  expanded: boolean;
+  selectedItem: {
+    [key: string]: any;
+  };
+  selectedValue: string | number;
+}
+
+const unknownObj = {
+  label: '?',
+  value: '',
+};
+
+export default class Options extends React.PureComponent<Props, State> {
   state = {
     expanded: false,
+    selectedItem: { ...unknownObj },
+    selectedValue: '',
   };
+
+  updateVisual = () => {};
+
+  componentDidMount() {}
 
   toggle = () => {
     this.setState((state) => ({
@@ -16,14 +39,51 @@ export default class Options extends React.PureComponent<{}, State> {
     }));
   };
 
+  selectItem = (value) => () => {
+    this.setState(
+      {
+        selectedValue: value,
+      },
+      () => {
+        if (this.props.onChange && typeof this.props.onChange === 'function') {
+          this.props.onChange(value);
+        }
+      }
+    );
+  };
+
+  renderItem = (item, index) => {
+    return (
+      <div
+        onClick={this.selectItem(item.value)}
+        className={
+          item.value === this.state.selectedValue
+            ? 'Options__item Options__item--selected'
+            : 'Options__item'
+        }
+        key={`${index}_`}
+      >
+        {item.label}
+      </div>
+    );
+  };
+
   render() {
     const className = this.state.expanded
       ? 'Options Options--expanded'
       : 'Options';
 
+    const selectedItem =
+      this.props.options.find(
+        (option) => option.value === this.state.selectedValue
+      ) || unknownObj;
     return (
       <div className={className} onClick={this.toggle}>
-        asd
+        {this.state.expanded
+          ? Array.isArray(this.props.options)
+            ? this.props.options.map(this.renderItem)
+            : 'Invalid options'
+          : selectedItem.label}
       </div>
     );
   }
