2968ea8455efeac46d81cdbc643c5fe57b2b1e06
Marq Roldan <hcmlroldan@gmail.com>
2020-06-14 17:46:52 +0800

    Updated Main component to show children

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,8 @@
 import './styles.scss';
 import React from 'react';
 
-
 export default class Main extends React.PureComponent {
-    render() {
-        return (<main className={'mainPrimary'}>hey</main>)
-    }
+  render() {
+    return <main className={'mainPrimary'}>{this.props.children}</main>;
+  }
 }
