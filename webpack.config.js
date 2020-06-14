5da1cd5bedcd3e531d22bb2af329ed3e6a07adb0
Marq Roldan <hcmlroldan@gmail.com>
2020-06-14 21:15:56 +0800

    Added assets path alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,57 @@
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-
   // webpack will take the files from ./src/index
   entry: './src/index',
 
   // and output it into /dist as bundle.js
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
 
   // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     alias: {
+      '@Assets': path.resolve(__dirname, 'src/assets'),
       '@Components': path.resolve(__dirname, 'src/components'),
       '@Styles': path.resolve(__dirname, 'src/styles'),
       '@Methods': path.resolve(__dirname, 'src/methods'),
-      '@Utils': path.resolve(__dirname, 'src/utils')
-    }
+      '@Utils': path.resolve(__dirname, 'src/utils'),
+    },
   },
 
   module: {
     rules: [
-
-        // we use babel-loader to load our jsx and tsx files
+      // we use babel-loader to load our jsx and tsx files
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         },
       },
 
       // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
       {
         test: /\.(sa|sc|c)ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       { test: /\.(png|jpg|jpeg|gif)$/, loader: 'url-loader?limit=100000' },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
-    ]
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+      template: './src/index.html',
+    }),
+  ],
 };
