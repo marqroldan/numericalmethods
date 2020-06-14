011e990e2c2ffe4b19f4e5bb1aff0327f013e9ef
Marq Roldan <hcmlroldan@gmail.com>
2020-06-14 12:55:54 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
 import '@Styles/main.scss';
 
 import React from 'react';
-import logo from './logo.svg';
-import RegulaFalsi from '@Methods/regulafalsi';
-import Secant from '@Methods/secant';
-import Bisection from '@Methods/bisection';
+
+import NumericalMethods from '@Methods';
 
 import Header from '@Components/Header';
 import Main from '@Components/Main';
+import Text from '@Components/Text';
 
 function App() {
-
-  const METHOD = Bisection;
+  const METHOD = NumericalMethods.Bisection;
   const method = new METHOD();
-  method.coefficients = [
-    -10,1,-4,1
-  ];
+  method.coefficients = [-10, 1, -4, 1];
 
   method.roundingRules = 4;
-
 
   method.process(5, 6);
 
   return (
     <div className="_container">
       <Header>
-        Inserted text
+        <div style={{ display: 'flex', flex: 1 }}>
+          <input type={'text'} />
+          <Text className={'label'}>COEFFICIENTS</Text>
+        </div>
+        <input type={'text'} />
       </Header>
-      <Main>
-
-      </Main>
+      <Main></Main>
     </div>
   );
 }
