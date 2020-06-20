6946b3012b5e92211a06209958db9e79fbaea049
Marq Roldan <hcmlroldan@gmail.com>
2020-06-21 05:05:58 +0800

    #1 Fixed PossibleRoots rerendering process

--- src/components/PossibleRoots/index.tsx
+++ src/components/PossibleRoots/index.tsx
@@ -1,22 +1,44 @@
 import './styles.scss';
 import React from 'react';
 import { countPossibleRoots, coefficientsFactory } from '@Utils/math';
+import { IPossibleRoots } from '@Utils/math/interfaces';
 import Text from '@Components/Text';
 
 interface Props {
   coefficients: string;
 }
 
-export default class PossibleRoots extends React.Component<Props> {
-  render() {
+interface State {
+  possibleRoots: IPossibleRoots[];
+}
+
+export default class PossibleRoots extends React.Component<Props, State> {
+  state = {
+    possibleRoots: [] as IPossibleRoots[],
+  };
+
+  updateRoots = () => {
     const possibleRoots = countPossibleRoots(
       coefficientsFactory(this.props.coefficients)
     );
+    this.setState({ possibleRoots });
+  };
 
+  componentDidMount() {
+    this.updateRoots();
+  }
+
+  componentDidUpdate(prevProps: Readonly<Props>) {
+    if (this.props.coefficients !== prevProps.coefficients) {
+      this.updateRoots();
+    }
+  }
+
+  render() {
     return (
       <div className={'PossibleRoots'}>
         <Text className={'heading3'}>Possible Roots</Text>
-        {possibleRoots.map((roots, index) => {
+        {this.state.possibleRoots.map((roots, index) => {
           return (
             <div className={'PossibleRootsContainer'} key={`${index}_`}>
               <div className={'PossibleRoots__values'}>
